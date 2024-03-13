59c7906402bd044e9bbcd5f4044b684a4aadd250
olgapapova <olena89-gulena@mail.ru>
2024-03-13 15:48:15 +0100

    commit1

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,16 @@
-
+import './Portfolio.css';
 
 export const Portfolio = () => {
 
     return (
       
-      <div>MY PORTFOLIO</div>
+      <div className="Portfolio_conteyner">
+        <div className='Portfolio_hiader'></div>
+        <div id="shimmerWave">
+          Hello, I'm Olga.
+          I'm frontend developer.
+        </div>
+      </div>
     );
 
 };
